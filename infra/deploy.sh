b5fbdf815229e793161a3031f7ad094720b9ef5d
heisenberg <blitzkriegcode77@gmail.com>
2025-02-28 10:39:30 +0100

    added branch dev

---
+++ infra/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Variables
+RESOURCE_GROUP="myResourceGroup"
+LOCATION="eastus"
+BICEP_FILE="main.bicep"
+
+# Login to Azure
+az login
+
+# Set the subscription
+az account set --subscription "your-subscription-id"
+
+# Create a resource group
+az group create --name $RESOURCE_GROUP --location $LOCATION
+
+# Deploy the Bicep file
+az deployment group create --resource-group $RESOURCE_GROUP --template-file $BICEP_FILE
+
+echo "Deployment completed successfully."
